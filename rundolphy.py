30be21f2fbf38f72666715fcd2cd9c808789380f
tomdyson <tomdyson@fd8de62e-3201-0410-a478-afb93c7a2038>
2006-07-28 08:14:28 +0000

    test supports new 'operator' argument

--- rundolphy.py
+++ rundolphy.py
@@ -13,13 +13,44 @@ def testIndex():
 		title = doc[0]
 		contents = doc[2]
 		epoch_date = doc[1]
-		d = Document(title, title, 'Tom', epoch_date)
+		uri = doc[3]
+		d = Document(uri, title, 'Tom Dyson', epoch_date)
 		# tags and comments should have been stripped already
 		contents = strip_comments.sub('',contents)
 		d.raw_data = strip_tags.sub('',contents) 
 		stream = t.tokenizeAndStem(title + ' ' + contents)
 		d.tokens, d.length = t.calculateTokens(stream)
 		i.add(d)
+	i.close()
+	took = time.time() - start
+	print "indexed %s docs in %.3f seconds" % (len(docs), took)
+	print "(%.2f docs per second)" % (len(docs) / took)
+	
+def testReutersIndex():
+	i = Index('data/test.db')
+	t = Text()
+	# add documents to the index
+	docs = marshal.loads(open('test/full-reuters.db').read())
+	start = time.time()
+	count = 0
+	for doc in docs:
+		title = doc[0]
+		print 'dealing with ' + title
+		contents = doc[1]
+		try:
+			epoch = int(time.mktime(time.strptime(doc[2],'%d-%b-%Y')))
+		except:
+			epoch = int(time.time())
+		uri = title
+		d = Document(uri, title, 'Reuters', epoch)
+		# tags and comments should have been stripped already
+		stream = t.tokenizeAndStem(title + ' ' + contents)
+		d.tokens, d.length = t.calculateTokens(stream)
+		d.raw_data = contents
+		i.add(d)
+		count += 1
+		if count % 100 == 0:
+			print 'completed %s documents' % count
 	i.close()
 	took = time.time() - start
 	print "indexed %s docs in %.3f seconds" % (len(docs), took)
@@ -35,17 +66,17 @@ def testUnicodeIndex():
 	d.tokens = calculated_tokens
 	i.add(d)
 	
-def testSearch(term, summariser='simple'):
+def testSearch(term, summariser='simple', operator="AND"):
 	# search the index
-	i = Index('data/test.db')
+	i = Index('data/reuters.index')
 	print '\nSearching for %s...' % term
 	start = time.time()
-	results = i.search(term, summariser)
+	results = i.search(term, summariser, 1, 5, operator)
 	took = time.time() - start
-	for result in results:
-		print result['title'], result['score']
-		print result['summary']
-	print "search took %.6f seconds" % (took)
+	for hit in results['hits']:
+		print hit['title'], hit['score']
+		print hit['summary'], hit['str_modified']
+	print "search took %.6f seconds, with %s results" % (took, results['count'])
 
 def testSortByDate(term):
 	# search the index
@@ -57,7 +88,7 @@ def testSortByDate(term):
 	took = time.time() - start
 	for result in results:
 		print result['title'], result['score']
-		print result['summary']
+		print result['summary'], result['str_modified']
 	print "search took %.6f seconds" % (took)
 
 def testStats():	
@@ -71,7 +102,8 @@ def testStats():
 		print term
 
 if __name__ == "__main__":
-	testIndex()
-	testSearch('loveday')
+	#testIndex()
+	#testReutersIndex()
+	testSearch('german rumours')
 	#testSortByDate('music')
 	#testStats()
