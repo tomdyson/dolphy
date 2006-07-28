ae34d96a504510ebc24b136643cd2dae398f5a2c
tomdyson <tomdyson@fd8de62e-3201-0410-a478-afb93c7a2038>
2006-07-28 08:18:38 +0000

    First unit tests

--- test-dolphy.py
+++ test-dolphy.py
@@ -1,24 +1,49 @@
 import unittest
-from dolphy import *
+import dolphy
 import os
+
+EMPTYINDEX = '/tmp/dolphy-testindex.db'
 
 class IndexCreation(unittest.TestCase):
 	
 	def testCreateEmptyIndex(self):
-		
-		TESTINDEX = '/tmp/dolphy-testindex.db'
 		"""A new index file should be created if it didn't exist before"""
 		try:
-			os.remove(TESTINDEX)
+			os.remove(EMPTYINDEX)
 		except OSError:
 			pass
-		i = Index(TESTINDEX)
-		self.assert_(os.path.exists(TESTINDEX))
+		i = dolphy.Index(EMPTYINDEX)
+		i.close()
+		self.assert_(os.path.exists(EMPTYINDEX))
 
 class IndexManipulation(unittest.TestCase):
 	
-	def testSingleDocumentAddition(self):
-		pass
+	def setUp(self):
+		"""Create an index with a single known entry"""
+		try: os.remove(EMPTYINDEX)
+		except OSError: pass
+		URI = 'test1.txt'
+		title = 'Test 1'
+		author = 'Tom'
+		modified = 1146593534
+		content = 'The cow says moo'
+		d = dolphy.Document(URI, title, author, modified)
+		d.raw_content = content
+		d.tokens = {'The':[0], 'cow':[1], 'says':[2], 'moo':[3]}
+		d.length = 4
+		i = dolphy.Index(EMPTYINDEX)
+		i.add(d)
+		i.close()
+		
+	def tearDown(self):
+		"""Remove index detritus"""
+		try: os.remove(EMPTYINDEX)
+		except OSError: pass		
+		
+	def testDocumentRetrieval(self):
+		i = dolphy.Index(EMPTYINDEX)
+		results = i.search('cow')
+		self.assertEquals(len(results), 1)
 		
 if __name__ == "__main__":
     unittest.main()
