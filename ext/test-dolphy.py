ebd3d5ddb3bda21da034b6c461cfc07d4c83d24b
tomdyson <tomdyson@fd8de62e-3201-0410-a478-afb93c7a2038>
2007-01-24 15:24:29 +0000

    Many new tests

--- ext/test-dolphy.py
+++ ext/test-dolphy.py
@@ -9,7 +9,7 @@ import os
 
 SIMPLEINDEX = '/tmp/dolphy-testindex.db'
 
-class IndexCreation(unittest.TestCase):
+class IndexManipulation(unittest.TestCase):
 	
 	def testCreateSIMPLEINDEX(self):
 		"""A new index file should be created if it didn't exist before"""
@@ -22,21 +22,24 @@ class IndexCreation(unittest.TestCase):
 class SearchTests(unittest.TestCase):
 	
 	def setUp(self):
-		"""Create an index with a single known entry"""
+		"""Create an index with a two known entries"""
 		try: os.remove(SIMPLEINDEX)
 		except OSError: pass
-		uri = 'test1.txt'
-		modified = 1146593534
-		content = 'The cow says moo'
-		d = Document(uri, modified)
+		d = Document('test1.txt', 1146593534)
 		d.title = 'Cow speech'
 		d.author = 'Tom Dyson'
-		d.raw_content = content
-		d.tokens = {'The':[0], 'cow':[1], 'says':[2], 'moo':[3]}
+		d.raw_content = 'The cow declares moo'
 		d.my_custom_attribute = 42
+		d.tokens = {'The':[0], 'cow':[1], 'says':[2], 'moo':[3]}
 		d.length = 4
+		d2 = Document('test2.txt', 1146593534)
+		d2.title = 'Sheep speech'
+		d2.author = 'Tom Dyson'
+		d2.raw_content = 'The sheep declares baa'
+		d2.tokens = {'The':[0], 'bull':[1], 'says':[2], 'moo':[3]}
+		d2.length = 4
 		self.i = Index(SIMPLEINDEX)
-		self.i.add(d)
+		self.i.add(d); self.i.add(d2)
 		
 	def tearDown(self):
 		"""Remove index detritus"""
@@ -46,27 +49,52 @@ class SearchTests(unittest.TestCase):
 		except OSError: pass		
 		
 	def testSingleTermQuery(self):
+		"""Single matching term should return one document"""
 		results = self.i.search('cow')
 		self.assertEquals(len(results.hits), 1)
+
+	def testSingleTermQuery2(self):
+		"""Single matching term should return two documents"""
+		results = self.i.search('moo')
+		self.assertEquals(len(results.hits), 2)
 
 	def testTwoTermUnionSuccessQuery(self):
 		"""'OR' queries with one matching term and one non-matching should return one document"""
 		results = self.i.search('cow q1w2e3r4', operator="OR")
 		self.assertEquals(len(results.hits), 1)
 
-	def testTwoTermUnionFailureQuery(self):
-		"""'OR' queries with two non-matching terms should return zero documents"""
-		results = self.i.search('c1e2d q1w2e3r4', operator="OR")
+	def testTwoTermUnionSuccessQuery2(self):
+		"""'OR' queries with one matching term should return two documents"""
+		results = self.i.search('cow bull', operator="OR")
+		self.assertEquals(len(results.hits), 2)
+
+	def testOperatorOverride(self):
+		"""Default 'OR' operator should be overridden by 'and' in query"""
+		results = self.i.search('cow and bull', operator="OR")
 		self.assertEquals(len(results.hits), 0)
+
+	def testOperatorOverride2(self):
+		"""Default 'AND' operator should be overridden by 'or' in query"""
+		results = self.i.search('cow or bull', operator="AND")
+		self.assertEquals(len(results.hits), 2)
 
 	def testTwoTermIntersectionSuccessQuery(self):
 		"""'AND' queries with two matching terms should return one document"""
 		results = self.i.search('cow moo', operator="AND")
 		self.assertEquals(len(results.hits), 1)
 		
+	def testSingleTermFailureQuery(self):
+		"""Single non-matching term should return zero documents"""
+		results = self.i.search('q1w2e3r4t5')
+		self.assertEquals(len(results.hits), 0)
+
+	def testTwoTermUnionFailureQuery(self):
+		"""'OR' queries with two non-matching terms should return zero documents"""
+		results = self.i.search('c1e2d q1w2e3r4', operator="OR")
+		self.assertEquals(len(results.hits), 0)
+		
 	def testThreeTermIntersectionFailureQuery(self):
-		"""'AND' queries with one non-matching term and multiple matching terms 
-			should return zero documents"""
+		"""'AND' queries with one non-matching term and multiple matching terms should return zero documents"""
 		results = self.i.search('cow q1w2e3r4 moo', operator="AND")
 		self.assertEquals(len(results.hits), 0)
 		
@@ -77,13 +105,12 @@ class SearchTests(unittest.TestCase):
 		self.assertEquals(len(results.hits), 0)
 				
 	def testCustomAttributeRetrieval(self):
+		"""Custom attributes should be retrieved"""
 		results = self.i.search('cow')
 		self.assertEquals(results.hits[0].my_custom_attribute, 42)
 		
-	def testZeroDocumentsReturned(self):
-		"""Single non-matching term should return zero documents"""
-		results = self.i.search('q1w2e3r4t5')
-		self.assertEquals(len(results.hits), 0)
-		
 if __name__ == "__main__":
-    unittest.main()
+	index_manipulation_suite = unittest.makeSuite(IndexManipulation)
+	unittest.TextTestRunner(verbosity=2).run(index_manipulation_suite)
+	search_suite = unittest.makeSuite(SearchTests)
+	unittest.TextTestRunner(verbosity=2).run(search_suite)
